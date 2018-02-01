10e4f057b1336f8a583949549e629c456dfe38f7
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-01 17:05:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(katzDeliLine){
   //This function should return the first person in line and then remove that individual from the line.
   //If there is nobody in line, it should return "There is nobody waiting to be served!"
   if(katzDeliLine.length !== 0){
-    return `Currently serving ${katzDeliLine.shift()}.`;`
+    return `Currently serving ${katzDeliLine.shift()}.`;
   } else {
     return "There is nobody waiting to be served!";
   }
