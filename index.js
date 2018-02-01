b614bad45dd1ae7f1b55d099dca1e7befe75b608
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-01 16:56:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, customer){
   //The function should return their position in line. And don't go being too programmer-y and give them their index. These are normal people. If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.
   katzDeliLine.push(customer);
-  var last = katzDeliLine[katzDeliLine.length];
+  var last = katzDeliLine.length;
   return `Welcome, ${customer}. You are number ${last} in line.`;
 }
