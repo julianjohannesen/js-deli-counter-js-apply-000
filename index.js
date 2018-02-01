8557e7a68d26c02320abfb4bf7134a858c8a7cf6
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-01 17:53:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,16 @@ function nowServing(katzDeliLine){
     return "There is nobody waiting to be served!";
   }
 }
+
+//3. Build a function `currentLine` that returns the current line. 
+function currentLine(katzDeliLine){
+  //For example, if `katzDeliLine` is currently `["Ada", "Grace"]`, `currentLine(katzDeliLine)` would return `"The line is currently: 1. Ada, 2. Grace"`.  If there is nobody in line, it should return `"The line is currently empty."`
+  var arr = [];
+  if(katzDeliLine.length !== 0){
+    for (var i = 0; i < katzDeliLine.length; i++){
+      arr[i] = `${katzDeliLine[i + 1]}. ${katzDeliLine[i]}`; 
+    }
+    return "The line is currenlty: " + arr.join(', ') + ".";
+  } else {
+    return "The line is currently empty.";
+  }
